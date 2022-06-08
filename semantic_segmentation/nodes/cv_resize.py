7e4ffabbd7a342de57bc806a42e033e28af1074f
Geoff McIver <geoff@p0wdr.com>
2022-06-08 16:09:26 +1200

    just use open cv because its faster

--- semantic_segmentation/nodes/cv_resize.py
+++ semantic_segmentation/nodes/cv_resize.py
@@ -5,9 +5,12 @@ Nearest neighbor resize using numpy only.
 Unfortunately the ROS-provided cv2.so is fantastically compiled without python3 bindings so it cannot be used with python3. This allows people who only need nearest neighbor resize to avoid these shenanigans entirely and avoid OpenCV dependency.
 
 Author: dheera@dheera.net
+Author: geoff@p0wdr.com - opencv is way faster....  just install it
+```pip3 install opencv-python```
 """
 
-import numpy as np
+
+import cv2
 
 def resize(img, new_shape):
     """
@@ -16,12 +19,4 @@ def resize(img, new_shape):
     shape = (width, height) as per OpenCV convention NOT numpy convention
     """
 
-    new_width, new_height = new_shape  # cv2 convention
-    old_height, old_width = img.shape[0], img.shape[1]  # numpy shenanigans
-
-    new_i, new_j = np.meshgrid(
-        np.linspace(0, old_width - 1, new_width).astype(np.uint16),
-        np.linspace(0, old_height -1, new_height).astype(np.uint16)
-    )
-
-    return img[new_j, new_i]
+    return cv2.resize(img, dsize=new_shape, interpolation=cv2.INTER_LINEAR)
